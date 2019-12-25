a0e437a7536c2222b5842b3160a7dd17842d5c3c
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-12-25 15:32:16 +0600

    Create swaptwo number .asm

---
+++ swaptwo number .asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+.data
+    a dw 1234
+    ;b dq 123456789
+.code
+main proc
+    
+    mov bl , 49
+    mov bh , 50
+    
+    xchg bl , bh ; exchange the value of bh and bl
+    
+    mov ah , 2
+    mov dl , bl
+    int 21h
+    
+    mov dl , 0ah
+    int 21h
+    mov dl , 0dh
+    int 21h
+    
+    mov dl , bh
+    int 21h
+    
+    
+    mov ah , 4ch
+    int 21h
+    main endp 
+end main
