8bac389fc0afb83c6fc9ddc25157e9e39c8f8c63
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-12-25 15:02:45 +0600

    Create inputoutput .asm

---
+++ inputoutput .asm
@@ -0,0 +1,18 @@
+.model small
+.stack 100h
+.data
+
+.code
+main proc
+    
+    mov ah , 1
+    int 21h
+    
+    mov ah , 2
+    mov dl , al
+    int 21h
+    
+    mov ah , 4ch
+    int 21h
+    main endp
+end main
